fa3f2c1a2e273cef81e1c86bab7497a204d199f5
Water <oboyiloejeh@gmail.com>
2026-02-16 23:46:11 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -18,7 +18,7 @@ self.addEventListener('install', (event) => {
   );
 });
 
-// Fetch event - serve from cache, fallback to network
+// Fetch event - serve from cache
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
